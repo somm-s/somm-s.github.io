e2350100e3b24e8a21dc6f6db9c64e11a70d06d0
Simon Sommerhalder <simonsom@student.ethz.ch>
2021-05-19 10:13:33 +0200

    new experiments

--- update_experiments.sh
+++ update_experiments.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm experiment*
-for i in {2..6}
+for i in {2..8}
 do
   cp ~/Documents/Bachelor_Thesis/repos/tf-experiments/all_experiments/experiment_results/experiment$i/experiment* .
 done
